fc514935532b3983cf45aa3a55819c38e6f156c8
viperscape <chrisfgill@gmail.com>
2017-04-21 16:46:03 -0400

    update for better highlighting

--- lichen.el
+++ lichen.el
@@ -4,18 +4,29 @@
 (setq font-lock-keywords
       `(
         (, "#.+" . font-lock-comment-face)
+        (, "\:\\w+" . font-lock-constant-face)
         (, "^\\w+.+" . font-lock-function-name-face)
         (,  "\".+\"" . font-lock-string-face)
 
-        (, "'" . font-lock-string-constant-face)
-        (, "\\s\"" . font-lock-string-delimiter-face)
+        (, "['|'!]" . font-lock-negation-char-face)
 
+        ;(, "\\(next.\\).+." . font-lock-variable-name-face)
         (, lkeywordsr . font-lock-keyword-face)
+        
+        ;(, "\\s\"" . font-lock-string-delimiter-face)
         ))
 
-(define-derived-mode lichen-mode c-mode "lichen"
+
+(defun lichen-mode ()
   "Major mode for editing lichen DSL"
-  (setq font-lock-defaults '((font-lock-keywords))))
+  (interactive)
+  (kill-all-local-variables)
+  
+  (set-syntax-table text-mode-syntax-table)
+  (set (make-local-variable 'font-lock-defaults) '(font-lock-keywords))
+  (setq major-mode 'lichen-mode)
+  (setq mode-name "lichen")
+  (run-hooks 'lichen-mode-hook))
 
 
 (add-to-list 'auto-mode-alist '("\\.ls\\'" . lichen-mode))
