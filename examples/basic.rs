1ebacd5731e41b462be1beb62b9f180c5cdd276c
viperscape <chrisfgill@gmail.com>
2017-06-09 17:50:22 -0400

    use empty

--- examples/basic.rs
+++ examples/basic.rs
@@ -2,25 +2,8 @@ extern crate lichen;
 
 use lichen::parse::Parser;
 use lichen::var::Var;
-use lichen::eval::{Eval,Evaluator};
+use lichen::eval::{Evaluator,Empty};
 
-struct Data; // we'll just make up an empty struct
-
-impl Eval for Data { // implement the required traits, but to keep it simple we wont actually do anything
-    #[allow(unused_variables)]
-    fn get (&self, path: Option<Vec<&str>>, lookup: &str) -> Option<Var> {
-        None
-    }
-
-    #[allow(unused_variables)]
-    fn set (&mut self, path: Option<Vec<&str>>, lookup: &str, var: Var) {
-    }
-
-    #[allow(unused_variables)]
-    fn call (&mut self, var: Var, fun: &str, vars: &Vec<Var>) -> Option<Var> {
-        None
-    }
-}
 
 fn main() {
     //load the lichen source file as a string
@@ -29,8 +12,8 @@ fn main() {
     
     let mut env = Parser::parse_blocks(src.to_mut()).expect("ERROR: Unable to parse source").into_env(); //parse the source and build the environment
 
-    let mut data = Data;
-    let mut ev = Evaluator::new(&mut env, &mut data); // build the evaluator based on the environment and any data
+    let mut empty = Empty;
+    let mut ev = Evaluator::new(&mut env, &mut empty); // build the evaluator based on the environment and any data
     
     while let Some((vars, _next_node)) = ev.next() { // here we loop through the evaluator steps
         for var in vars {
